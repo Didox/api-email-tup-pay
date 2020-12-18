49eb4c24fd2f8ef923a2751a5001246e701d7485
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-12-17 23:05:50 -0300

    deploy java

--- src/main/java/br/com/tornese/controller/EmailController.java
+++ src/main/java/br/com/tornese/controller/EmailController.java
@@ -1,13 +1,14 @@
 package br.com.tornese.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin("*")
 public class EmailController {
+	
 	@PostMapping("/email/enviar")
 	public String enviar() {
 		return "{\"status\": \"Email enviado com sucesso\", \"id_transacao\": \"TSGJ12653\"}";
