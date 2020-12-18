008267a61e03cbb6ec7d0205ac5bd6ea35250b69
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-12-17 22:54:50 -0300

    fix

--- src/main/java/br/com/tornese/controller/EmailController.java
+++ src/main/java/br/com/tornese/controller/EmailController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin("*")
 public class EmailController {
-	@PostMapping("/")
+	@PostMapping("/email/enviar")
 	public String enviar() {
 		return "{\"status\": \"Email enviado com sucesso\", \"id_transacao\": \"TSGJ12653\"}";
 	}
